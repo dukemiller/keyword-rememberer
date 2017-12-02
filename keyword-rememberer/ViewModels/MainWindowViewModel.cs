11f110519b645a5f508d05795cb76f6ac36925f7
dukemiller <dukemiller928@hotmail.com>
2017-12-02 14:36:57 -0700

    Add settings flyout and command+property in viewmodel to access it
    Add a 'loaded' event for the autosave functionality as well

--- keyword-rememberer/ViewModels/MainWindowViewModel.cs
+++ keyword-rememberer/ViewModels/MainWindowViewModel.cs
@@ -11,8 +11,26 @@ namespace keyword_rememberer.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        public static bool Loaded { get; set; }
 
-        public MainWindowViewModel(ISettingsRepository settingsRepository) {}
+        public MainWindowViewModel(ISettingsRepository settingsRepository)
+        {
+            LoadedCommand = new RelayCommand(() => Loaded = true);
+            OpenSettingsCommand = new RelayCommand(() => SettingsIsOpen ^= true);
+            SettingsRepository = settingsRepository;
+        }
 
+        // 
+
+        public string Words { get; set; }
+
+        public bool SettingsIsOpen { get; set; }
+
+        public RelayCommand LoadedCommand { get; set; }
+
+        public RelayCommand OpenSettingsCommand { get; set; }
+
+        public ISettingsRepository SettingsRepository { get; set; }
+        
     }
 }
