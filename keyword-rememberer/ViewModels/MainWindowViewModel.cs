068b10fe3e7287a57661f7397240b570ac054eba
dukemiller <dukemiller928@hotmail.com>
2017-12-02 13:52:09 -0700

    Initial project structure & add nuget packages

---
+++ keyword-rememberer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using keyword_rememberer.Enums;
+using keyword_rememberer.Repositories.Interface;
+
+namespace keyword_rememberer.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        public MainWindowViewModel(ISettingsRepository settingsRepository) {}
+
+    }
+}
