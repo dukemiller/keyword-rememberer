e3fa9d99227e278cf834e9f1c579d5f35856f6b8
dukemiller <dukemiller928@hotmail.com>
2017-12-02 13:53:49 -0700

    Add simple viewmodellocator

---
+++ keyword-rememberer/ViewModelLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using GalaSoft.MvvmLight.Ioc;
+using keyword_rememberer.Repositories;
+using keyword_rememberer.Repositories.Interface;
+using keyword_rememberer.ViewModels;
+using Microsoft.Practices.ServiceLocation;
+
+namespace keyword_rememberer
+{
+    public class ViewModelLocator
+    {
+
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            // Viewmodels
+            SimpleIoc.Default.Register<MainWindowViewModel>();
+        }
+
+        public MainWindowViewModel Main => SimpleIoc.Default.GetInstance<MainWindowViewModel>();
+    }
+}
