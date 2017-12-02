2e40347f2cf5efc3e65701a8dc8dfd99e62ecd49
dukemiller <dukemiller928@hotmail.com>
2017-12-02 14:08:28 -0700

    Add some basic converters for the view later on

---
+++ keyword-rememberer/Classes/Converters.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace keyword_rememberer.Classes
+{
+    public class PlusOneConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToInt32(value) + 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BoolVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToBoolean(value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    ///     Joins an array of strings delimited by a comma.
+    /// </summary>
+    public class StringJoinConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var enumerable = value as IEnumerable<string> ?? new List<string>();
+            return string.Join(", ", enumerable.Where(s => !string.IsNullOrEmpty(s)));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = value as string ?? "";
+            return str.Split(new[] { ", " }, StringSplitOptions.None);
+        }
+    }
+}
